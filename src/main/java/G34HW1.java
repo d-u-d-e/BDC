221a60e6ac10b55498a77ce89fb893018b32a880
Fabio Vaccaro <fabiovaccaro1@gmail.com>
2021-03-31 11:30:01 +0200

    Better formatting, new comments and output.txt file
    I have formatted the code and I have deleted unnecessary comments and I have added new ones. For me this is the final version to be submitted.
    
    Also added the output.txt file to the repo.

--- src/main/java/G34HW1.java
+++ src/main/java/G34HW1.java
@@ -17,87 +17,70 @@ public class G34HW1{
     public static final String inputFile = "input_20K.csv";
     public static void main(String[] args) throws IOException {
 
-        // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-        // CHECKING NUMBER OF CMD LINE PARAMETERS
-        // Parameters are: num_partitions, <path_to_file>
-        // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-
+        //Checks the input parameters number
         if (args.length != 3) {
-            throw new IllegalArgumentException("USAGE: num_partitions file_path");
+            throw new IllegalArgumentException("USAGE: num_partitions num_top_results output_file_path");
         }
 
-        // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-        // SPARK SETUP
-        // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-
+        //Sets Spark configuration
         SparkConf conf = new SparkConf(true).setAppName("Homework1");
         JavaSparkContext sc = new JavaSparkContext(conf);
         sc.setLogLevel("WARN");
 
-        // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-        // INPUT READING
-        // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-
-        // Read number of partitions
+        //Reads number of partitions and number of desired top results
         int K = Integer.parseInt(args[0]);
         int T = Integer.parseInt(args[1]);
 
-        // Read input file and subdivide it into K random partitions
+        //Reads input file and subdivide it into K random partitions
         JavaRDD<String> rawData = sc.textFile(inputFile).repartition(K).cache();
 
-        // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-        // SETTING GLOBAL VARIABLES
-        // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-
-
+        /* NORMALIZED RATING RDD */
         JavaPairRDD<String, Float> normalizedRating;
-
-        // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-        // STANDARD WORD COUNT with reduceByKey
-        // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-
         normalizedRating = rawData
                 .mapToPair((review) -> {    // <-- MAP PHASE (R1)
                     String[] tokens = review.split(",");
-                    return new Tuple2<>(tokens[1],tokens[0]+","+tokens[2]);
+                    return new Tuple2<>(tokens[1], tokens[0] + "," + tokens[2]);
                 })
                 .groupByKey() // <-- REDUCE PHASE (R1)
-                .flatMapToPair( (element) -> {
+                .flatMapToPair((element) -> {
                     ArrayList<Tuple2<String, Float>> products = new ArrayList<>();
                     float mean = 0;
                     int counter=0;
                     for (String s : element._2()){
                         String[] list= s.split(",");
                         Float rating =Float.parseFloat(list[1]);
-                        products.add(new Tuple2<>( list[0],rating ));
-                        mean+= rating;
+                        products.add(new Tuple2<>(list[0], rating));
+                        mean += rating;
                         counter++;
                     }
-                    mean/=counter;
+                    mean /= counter;
                     ArrayList<Tuple2<String, Float>> normProducts = new ArrayList<>();
-                    for (Tuple2<String, Float> t : products ){
-                        normProducts.add(new Tuple2<>( t._1 , t._2-mean));
+                    for (Tuple2<String, Float> t : products){
+                        normProducts.add(new Tuple2<>(t._1, t._2-mean));
                     }
                     return  normProducts.iterator();
                 });
-        JavaPairRDD<String, Float> maxNormRatings = normalizedRating.reduceByKey( (x, y) -> Math.max(x, y) );
 
-                //.mapToPair( (element) ->  new Tuple2<>(element._2,element._1))
-        List<Tuple2<Float, String>> topList= maxNormRatings.mapToPair(x -> x.swap())
+        /* MAX NORMALIZED RATING RDD */
+        JavaPairRDD<String, Float> maxNormRatings = normalizedRating.reduceByKey((x, y) -> Math.max(x, y));
+
+        /* MAX NORMALIZED RATING RDD (REVERSED) */
+        List<Tuple2<Float, String>> topList = maxNormRatings
+                .mapToPair(x -> x.swap())
                 .sortByKey(false)
                 .take(T);
+
+        //Creates an output file if it not exists and opens a FileWriter
         File f = new File(args[2]);
-        boolean fExists =f.createNewFile();
+        f.createNewFile();
+        FileWriter fw = new FileWriter(args[2]);
 
-        FileWriter fw = new FileWriter( args[2] );
-
-        fw.write("INPUT PARAMETERS: K="+K+" T="+T+" file="+inputFile+"\n\nOUTPUT:\n" );
-        for(Tuple2<Float, String> t : topList ){
-            fw.write("Product "+t._2+" maxNormRating "+t._1+"\n");
+        //Writes to the output
+        fw.write("INPUT PARAMETERS: K=" + K + " T=" + T + " file=" + inputFile + "\n\nOUTPUT:\n");
+        for (Tuple2<Float, String> t : topList) {
+            fw.write("Product " + t._2 + " maxNormRating " + t._1 + "\n");
         }
         fw.close();
-
-
     }
 
 }
