ed132e5a7b456a55f891cecac69b97bfc7b05add
WittyDude <mari8.marco@gmail.com>
2021-04-09 17:55:53 +0200

    fixed program parameter

--- src/main/java/G34HW1.java
+++ src/main/java/G34HW1.java
@@ -2,24 +2,19 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.sql.types.FloatType;
-import org.apache.spark.storage.StorageLevel;
+
 import scala.Tuple2;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
 
 public class G34HW1{
 
-    public static final String inputFile = "input_20K.csv";
     public static void main(String[] args) throws IOException {
 
         //Checks the input parameters number
         if (args.length != 3) {
-            throw new IllegalArgumentException("USAGE: num_partitions num_top_results output_file_path");
+            throw new IllegalArgumentException("USAGE: num_partitions num_top_results input_file_path");
         }
 
         //Sets Spark configuration
@@ -30,6 +25,7 @@ public class G34HW1{
         //Reads number of partitions and number of desired top results
         int K = Integer.parseInt(args[0]);
         int T = Integer.parseInt(args[1]);
+        String inputFile = args[2];
 
         //Reads input file and subdivide it into K random partitions
         JavaRDD<String> rawData = sc.textFile(inputFile).repartition(K).cache();
@@ -45,10 +41,10 @@ public class G34HW1{
                 .flatMapToPair((element) -> {
                     ArrayList<Tuple2<String, Float>> products = new ArrayList<>();
                     float mean = 0;
-                    int counter=0;
+                    int counter = 0;
                     for (String s : element._2()){
-                        String[] list= s.split(",");
-                        Float rating =Float.parseFloat(list[1]);
+                        String[] list = s.split(",");
+                        Float rating = Float.parseFloat(list[1]);
                         products.add(new Tuple2<>(list[0], rating));
                         mean += rating;
                         counter++;
@@ -56,7 +52,7 @@ public class G34HW1{
                     mean /= counter;
                     ArrayList<Tuple2<String, Float>> normProducts = new ArrayList<>();
                     for (Tuple2<String, Float> t : products){
-                        normProducts.add(new Tuple2<>(t._1, t._2-mean));
+                        normProducts.add(new Tuple2<>(t._1, t._2 - mean));
                     }
                     return  normProducts.iterator();
                 });
@@ -70,17 +66,9 @@ public class G34HW1{
                 .sortByKey(false)
                 .take(T);
 
-        //Creates an output file if it not exists and opens a FileWriter
-        File f = new File(args[2]);
-        f.createNewFile();
-        FileWriter fw = new FileWriter(args[2]);
-
-        //Writes to the output
-        fw.write("INPUT PARAMETERS: K=" + K + " T=" + T + " file=" + inputFile + "\n\nOUTPUT:\n");
+        System.out.println("INPUT PARAMETERS: K=" + K + " T=" + T + " file=" + inputFile + "\n\nOUTPUT:");
         for (Tuple2<Float, String> t : topList) {
-            fw.write("Product " + t._2 + " maxNormRating " + t._1 + "\n");
+            System.out.println("Product " + t._2 + " maxNormRating " + t._1);
         }
-        fw.close();
     }
-
 }
